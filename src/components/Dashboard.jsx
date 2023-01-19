35bef9cd6e58f1e3c914e18cd7e95a9f6761d9ed
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-01-19 13:24:41 -0800

    add dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+
+import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm",
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday",
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3",
+  },
+];
+
+class Dashboard extends Component {
+  state = {
+    loading: false,
+    focused: null,
+    days: [],
+    appointments: [],
+    interviewers: [],
+  };
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  selectPanel (id) {
+
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+
+    // I'd originally used this code which is much more lengthy
+      // if (this.state.focused === null) {
+      //   this.setState({
+      //     focused: id
+      //   });
+      // } else {
+      //   this.setState({
+      //     focused: null
+      //   });
+      // }
+
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+       <Panel
+        key={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={() => this.selectPanel(panel.id)}
+       />
+      ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
+  }
+}
+
+export default Dashboard;
